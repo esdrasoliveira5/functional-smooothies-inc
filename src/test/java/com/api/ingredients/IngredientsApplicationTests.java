988893ecbaafb690ec2508f7ea7a32379cca7bc7
Esdras Oliveira <esdrasjesus5@gmail.com>
2022-10-29 12:29:28 -0300

    test: all teste cases implemented

--- src/test/java/com/api/ingredients/IngredientsApplicationTests.java
+++ src/test/java/com/api/ingredients/IngredientsApplicationTests.java
@@ -28,7 +28,7 @@ class IngredientsApplicationTests {
   @Order(1)
   @DisplayName("1 - Display the correct list os ingredients")
   void shouldBeReturnIngredients() throws Exception {
-    final var smoothie = new SmoothieDto("Classic,-strawberry,-peanut");
+    final var smoothie = new SmoothieDto("Classic,-strawberry");
     mockMvc
         .perform(post("/ingredients/create").contentType(MediaType.APPLICATION_JSON)
             .content(new ObjectMapper().writeValueAsString(smoothie)))
@@ -45,5 +45,55 @@ class IngredientsApplicationTests {
         .perform(post("/ingredients/create").contentType(MediaType.APPLICATION_JSON)
             .content(new ObjectMapper().writeValueAsString(smoothie)))
         .andExpect(status().isBadRequest()).andExpect(jsonPath("$.error").value("Empty Input"));
+  }
+
+  @Test
+  @Order(3)
+  @DisplayName("3 - Display the correct error message and status when Smoothie dont exist")
+  void shouldBeReturnErrorNotFound() throws Exception {
+    final var smoothie = new SmoothieDto("Very Tasty Smoothie,-strawberry,-peanut");
+    mockMvc
+        .perform(post("/ingredients/create").contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(smoothie)))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.error").value("Not Found Smoothie"));
+  }
+
+
+  @Test
+  @Order(4)
+  @DisplayName("4 - Display the correct error message and status when additional ingredient detected")
+  void shouldBeReturnErrorAdditionalIngredient() throws Exception {
+    final var smoothie = new SmoothieDto("Classic,mango");
+    mockMvc
+        .perform(post("/ingredients/create").contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(smoothie)))
+        .andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.error").value("Aditional Ingredient Detected"));
+  }
+
+
+  @Test
+  @Order(5)
+  @DisplayName("5 - Display the correct error message and status when atype of input is wrong")
+  void shouldBeReturnErrorWhenWrongTypeOfInput() throws Exception {
+    final var smoothie = new SmoothieDto("1");
+    mockMvc
+        .perform(post("/ingredients/create").contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(smoothie)))
+        .andExpect(status().isBadRequest());
+
+  }
+
+  @Test
+  @Order(6)
+  @DisplayName("6 - Display the correct list os ingredients and ignore allergens imgredients that do not is present of the smoothie ingredients")
+  void shouldBeReturnIngredientsAndIgnoreIngredients() throws Exception {
+    final var smoothie = new SmoothieDto("Classic,-strawberry,-peanut,-blackberry");
+    mockMvc
+        .perform(post("/ingredients/create").contentType(MediaType.APPLICATION_JSON)
+            .content(new ObjectMapper().writeValueAsString(smoothie)))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.ingredient").value("banana,pineapple,mango,peach,honey"));
   }
 }
